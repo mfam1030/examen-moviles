9ba4a5e1c16e7461d9951da9868cf37ffac0b8fa
Maria Aguero <mariaguero745@gmail.com>
2025-03-05 18:35:22 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Buscador de repositorios',
+  webDir: 'dist'
+};
+
+export default config;
